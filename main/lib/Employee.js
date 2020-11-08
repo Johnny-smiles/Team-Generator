19f40c189a0d9d55929b50c41a3466cf43fbc9da
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-11-08 17:43:17 -0600

    added employee constructor

--- main/lib/Employee.js
+++ main/lib/Employee.js
@@ -1,5 +1,23 @@
+// creating class constructor to be the main basis for the three 
+// employee types
+
 class Employee {
     constructor(name, id, email){
-        
+        // properties of employee object
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    // method added for accessing name
+    getName() {
+        return this.name;
+    }
+    // method added for accessing id
+    getId() {
+        return this.id;
+    }
+    // method added for accessing email
+    getEmail() {
+        return this.email
     }
 }
