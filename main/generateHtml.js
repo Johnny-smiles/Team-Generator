ae79a0845b472ebff8ad56b20ab6e0b7cdee95f9
Johnny Smiles <70188711+Johnny-smiles@users.noreply.github.com>
2020-11-08 21:46:54 -0600

    Added html formating and jqueury to populate data points

--- main/generateHtml.js
+++ main/generateHtml.js
@@ -1,5 +1,9 @@
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+
 // generate html
-function generateHtml(data) {
+function generateHtml(teamMember) {
+
   return `
 
   <!DOCTYPE html>
@@ -7,10 +11,40 @@ function generateHtml(data) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Profile</title>
+    <title>Team profiles</title>
 </head>
 <body>
-    
+    <div>
+        <header><h1>Team profiles</h1></header>
+     </div>
+<div>
+    <div><h2>Team Members</h2></div>
+    <div>
+        <h3>Manager</h3>
+        <ul>
+            <li>${teamMember.Manager.getName()}</li>
+            <li>${teamMember.Manager.getId()}</li>
+            <li>${teamMember.Manager.getEmail()}</li>            
+            <li>${teamMember.Manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+    <div>
+        <h3>Engineer</h3>
+        <ul>
+            <li>${teamMember.Engineer.getName()}</li>
+            <li>${teamMember.Engineer.getId()}</li>
+            <li>${teamMember.Engineer.getEmail()}</li>
+            <li>${teamMember.Engineer.getGitHub()}</li>
+        </ul>
+        <h3>Intern</h3>
+        <ul>
+            <li>${teamMember.Intern.getName()}</li>
+            <li>${teamMember.Intern.getId()}/li>
+            <li>${teamMember.Intern.getEmail()}</li>
+            <li>${teamMember.Intern.getSchool()}</li>
+        </ul>
+    </div>
+</div>
 </body>
 </html>
 
--- main/index.js
+++ main/index.js
@@ -8,9 +8,11 @@ const  generateHtml = require('./generateHtml');
 const inquirer = require('inquirer')
 const fs = require('fs');
 
+const teamMembers = [];
+
 // function to write html file
-const writeFile = answers => {
-fs.writeFileSync('./develop/dist/Profile.html', generateHtml(answers))
+const createHtml = teamMembers => {
+fs.writeFileSync('./develop/dist/Profile.html', generateHtml(teamMembers))
 
 }
 
@@ -22,50 +24,195 @@ const init = () => {
   inquirer.prompt([
         {
             type: 'input',
-            name: 'name',
-            message: 'What is your name? (Required)',
-            validate: githubInput => {
-                if (githubInput) {
+            name: 'managerName',
+            message: `What is your Manager's name? (Required)`,
+            validate: nameInput => {
+                if (nameInput) {
                   return true;
                 } else {
-                  console.log('Please enter your name.');
+                  console.log(`Please enter your manager's name.`);
                   return false;
                 }
               }
            },
            {
             type: 'input',
-            name: 'project',
-            message: 'Enter your project name (Required)',
-            validate: githubInput => {
-                if (githubInput) {
+            name: 'managerId',
+            message: `What is your Manager's Id? (Required)`,
+            validate: IdInput => {
+                if (IdInput) {
                   return true;
                 } else {
-                  console.log("Please enter your project's name.");
+                  console.log(`Please enter your manager's Id.`);
                   return false;
                 }
               }
            },
            {
             type: 'input',
-            name: 'about',
-            message: 'What is your project description (Required)',
-            validate: githubInput => {
-                if (githubInput) {
+            name: 'managerEmail',
+            message: `What is your Manager's Email? (Required)`,
+            validate: emailInput => {
+                if (emailInput) {
                   return true;
                 } else {
-                  console.log("Please enter your project's description.");
+                  console.log(`Please enter your manager's Email.`);
                   return false;
                 }
               }
-         }        
+           },  
+           {
+            type: 'input',
+            name: 'managerOfficeNumber',
+            message: `What is your Manager's Office Number? (Required)`,
+            validate: officeNumberInput => {
+                if (officeNumberInput) {
+                  return true;
+                } else {
+                  console.log(`Please enter your manager's Office Number.`);
+                  return false;
+                }
+              }
+           },                
     ])
     .then(answers => {
         console.log("==============")
         console.log(answers)
-        writeFile(answers) 
         console.log("==============")
+        const manager = new Manager (answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber ) 
+        teamMembers.push(manager);
+        engineer()
     });
  };
+ const engineer = () => {
+  inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: `What is your engineer's name? (Required)`,
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log(`Please enter your engineer's name.`);
+                  return false;
+                }
+              }
+           },
+           {
+            type: 'input',
+            name: 'engineerId',
+            message: `What is your engineer's Id? (Required)`,
+            validate: IdInput => {
+                if (IdInput) {
+                  return true;
+                } else {
+                  console.log(`Please enter your engineer's Id.`);
+                  return false;
+                }
+              }
+           },
+           {
+            type: 'input',
+            name: 'engineerEmail',
+            message: `What is your engineer's Email? (Required)`,
+            validate: emailInput => {
+                if (emailInput) {
+                  return true;
+                } else {
+                  console.log(`Please enter your engineer's Email.`);
+                  return false;
+                }
+              }
+           },  
+           {
+            type: 'input',
+            name: 'engineerGithub',
+            message: `What is your engineer's GitHub? (Required)`,
+            validate: gitHubInput => {
+                if (gitHubrInput) {
+                  return true;
+                } else {
+                  console.log(`Please enter your engineer's Git.`);
+                  return false;
+                }
+              }
+           },                
+    ])
+    .then(answers => {
+        console.log("==============")
+        console.log(answers)
+        console.log("==============")
+        const engineer = new Engineer (answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerOfficeNumber ) 
+        teamMembers.push(engineer);
+        intern()
+    });
+ };
+ const intern = () => {
+  inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: `What is your intern's name? (Required)`,
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log(`Please enter your intern's name.`);
+                  return false;
+                }
+              }
+           },
+           {
+            type: 'input',
+            name: 'internId',
+            message: `What is your intern's Id? (Required)`,
+            validate: IdInput => {
+                if (IdInput) {
+                  return true;
+                } else {
+                  console.log(`Please enter your intern's Id.`);
+                  return false;
+                }
+              }
+           },
+           {
+            type: 'input',
+            name: 'internEmail',
+            message: `What is your intern's Email? (Required)`,
+            validate: emailInput => {
+                if (emailInput) {
+                  return true;
+                } else {
+                  console.log(`Please enter your intern's Email.`);
+                  return false;
+                }
+              }
+           },  
+           {
+            type: 'input',
+            name: 'internGithub',
+            message: `What is your intern's GitHub? (Required)`,
+            validate: gitHubInput => {
+                if (gitHubrInput) {
+                  return true;
+                } else {
+                  console.log(`Please enter your intern's Git.`);
+                  return false;
+                }
+              }
+           },                
+    ])
+    .then(answers => {
+        console.log("==============")
+        console.log(answers)
+        console.log("==============")
+        const intern = new Intern (answers.internName, answers.internId, answers.internEmail, answers.internOfficeNumber ) 
+        teamMembers.push(intern);
+        createHtml()
+    });
+ };
+
+ 
 
 init()
